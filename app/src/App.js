aae3c527cd60c72ceca5848191fece1551d83e0f
Jungeunkim-dev <svaha@ewhain.net>
2022-05-23 17:21:39 +0900

    build : 프로젝트 초기 세팅

---
+++ app/src/App.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+function LocationScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="마이페이지"
+        onPress={() =>
+          navigation.navigate('Mypage')}
+      />
+    </View>
+  );
+}
+
+function InitialSettingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="위치선택"
+        onPress={() =>
+          navigation.navigate('Location')}
+      />
+    </View>
+  );
+}
+
+function MypageScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="메인페이지"
+        onPress={() =>
+          navigation.navigate('Mainpage')}
+      />
+      </View>
+  );
+}
+
+function MainpageScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="마이페이지"
+        onPress={() =>
+          navigation.navigate('Mypage')}
+      />
+      </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+
+function Root() {
+  return (
+    <Stack.Navigator initialRouteName="InitialSettings">
+      <Stack.Screen name="Location" component={LocationScreen} />
+      <Stack.Screen name="InitialSettings" component={InitialSettingScreen} />
+      <Stack.Screen name="Mypage" component={MypageScreen} />
+      <Stack.Screen name="Mainpage" component={MainpageScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function App(){
+return(
+  <NavigationContainer>
+    <Root/>
+  </NavigationContainer>
+);
+}
+
+
+
+
+
+export default App;
