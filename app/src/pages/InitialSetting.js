9911963b8fa9aef6207cc3d0975002ce6a00efe2
gkim <kimkm0419@naver.com>
2022-05-25 22:54:07 +0900

    #3 Feat : Create Initial Setting Screen's features

--- app/src/pages/InitialSetting.js
+++ app/src/pages/InitialSetting.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+// import { Ionicons, AntDesign } from '@expo/vector-icons';
 
 
 const InitialSettingScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        title="위치선택"
-        onPress={() =>
-          navigation.navigate('Location')}
-      />
+    <View>
+      <Text>Initial Setting</Text>
+
+      <Text>ID</Text>
+      <Pressable
+        onPress={() => {}}
+      >
+        <Text>ewha</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={() => {}}
+      >
+        <Text>LOGOUT</Text>
+      </Pressable>
+
+      <Text>Location</Text>
+      <Pressable
+        onPress={() => navigation.navigate('Location')}
+      >
+        <Text>(location info)</Text>
+        {/* <Ionicons name="search-sharp"/> */}
+      </Pressable>
+      
+      <Text>Constitution</Text>
+      <Pressable>
+        <Text>Dropdown</Text>
+        {/* <AntDesign name="down"/> */}
+      </Pressable>
     </View>
   );
 }
