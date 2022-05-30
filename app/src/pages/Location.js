487af72b08b015ec1496c8554b70a4955f5b1fcd
Jungeunkim-dev <svaha@ewhain.net>
2022-05-30 16:20:13 +0900

    #2 feat : location 페이지 추가

---
+++ app/src/pages/Location.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+import SearchBar from '../components/SearchBar';
+
+const Location = () => {
+  return (
+    <SearchBar />
+  );
+};
+
+export default Location;
