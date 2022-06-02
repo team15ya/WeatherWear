1ac69c8223a3b4e4381a23d7f2752e5792fe2273
Jungeunkim-dev <svaha@ewhain.net>
2022-06-02 23:16:17 +0900

    #22 chore : fix typo

--- app/src/components/ModalView.js
+++ app/src/components/ModalView.js
@@ -36,7 +36,7 @@ const ModalView = () => {
       </View>
       <View style={{paddingTop:20}}>
         <TouchableOpacity style={styles.button} onPress={() => { Linking.openURL('https://www.musinsa.com/search/musinsa/integration?type=&q=HOODy') }}>
-          <Text style={{ fontSize: 20, color: 'white' }}>Goto Musinsa</Text>
+          <Text style={{ fontSize: 20, color: 'white' }}>Go to musinsa</Text>
         </TouchableOpacity>
       </View>
 
